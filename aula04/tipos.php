b65bd095f26c39c5183b606ffd673b10c7823d1f
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2024-01-22 13:13:49 -0300

    aula 5

---
+++ aula04/tipos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <<link rel="stylesheet" href="_css/estilo.css"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+            /*$n = 4;
+            $no = "Gustavo";
+            $n = 4.5;
+            echo $n;*/
+        
+            $nome = "Agnaldo";
+            $idade = 51;
+            //echo $nome." tem ".$idade." anos."
+            echo "$nome tem $idade anos!";
+        ?>
+    </div>
+</body>
+</html>
